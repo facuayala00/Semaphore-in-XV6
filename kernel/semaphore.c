bf4a9ee5e8698146a9926d47bfb94da34f11fe77
Benjamin Lozano <benjamin.lozano@mi.unc.edu.ar>
2022-09-21 20:41:47 -0300

    Iniciado semaforo?

---
+++ kernel/semaphore.c
@@ -0,0 +1,24 @@
+#include "types.h"
+#include "riscv.h"
+#include "defs.h"
+#include "param.h"
+#include "spinlock.h"
+#include "proc.h"
+#include "fs.h"
+#include "sleeplock.h"
+#include "file.h"
+#include <alloca.h>
+#include <stdlib.h>
+
+int *sem_array; //Array general
+
+int *sem_open(int *sem, int value){
+    sem = calloc(value, sizeof(int)); //Inicializamos sem con value elementos?
+    return sem;
+}
+
+int sem_close(int sem);
+
+int sem_up(int sem);
+
+int sem_down(int sem);
